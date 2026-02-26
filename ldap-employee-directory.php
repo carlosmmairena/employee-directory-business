ab9b42877e006560baaf58082dc3acd25a989717
Carlos Mairena <carlosmiguelmairena@gmail.com>
2026-02-26 12:09:04 -0600

    doc: set author and website

--- ldap-employee-directory.php
+++ ldap-employee-directory.php
@@ -6,8 +6,8 @@
  * Version:     1.0.1
  * Requires at least: 5.8
  * Requires PHP: 7.4
- * Author:      Your Name
- * Author URI:  https://example.com
+ * Author:      Carlos Mairena
+ * Author URI:  https://carlosmmairena.com
  * License:     GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ldap-employee-directory
